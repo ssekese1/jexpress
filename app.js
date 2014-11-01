14be838287ee02d4e29e98508dc862ddc46d8cc4
Jason Norwood-Young <jason@10layer.com>
2014-11-01 10:08:23 +0200

    CORS

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('./config');
 var routes = require('./app/routes/index');
+var cors = require('cors');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(cors());
 
 //DB connection
 var mongoose   = require('mongoose');
