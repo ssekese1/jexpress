4061d46dcfb7c33b22e8f1c50c76b507f596dd7a
Jason Norwood-Young <jason@10layer.com>
2014-11-09 12:05:07 +0200

    Handle Reserve from Booking Model

--- app/models/booking_model.js
+++ app/models/booking_model.js
@@ -9,6 +9,7 @@ var BookingSchema   = new Schema({
 	end_time: Date,
 	title: String,
 	member: String,
+	cost: Number,
 	_owner_id: Objectid
 });
 
@@ -16,6 +17,49 @@ BookingSchema.set("_perms", {
 	admin: "crud",
 	owner: "crud",
 	user: "cr",
+});
+
+BookingSchema.post("save", function(transaction) { //Keep our running total up to date
+	try {
+		var Reserve = require("./reserve_model");
+		var reserve = Reserve({
+			user_id: transaction._owner_id,
+			description: "Booking",
+			amount: transaction.cost * -1,
+			cred_type: "space",
+			source_type: "booking",
+			source_id: transaction._id
+		});
+		reserve.save();
+	} catch(err) {
+		console.log("Error", err);
+		// throw(err);
+	}
+});
+
+BookingSchema.post("remove", function(transaction) { //Keep our running total up to date
+	console.log("Going to remove reserve");
+	console.log(transaction);
+	try {
+		var Reserve = require("./reserve_model");
+		Reserve.findOne({
+			source_type: "booking",
+			source_id: transaction._id
+		}, function(err, item) {
+			if (err) {
+				console.log("Error", err);
+				return;
+			}
+			if (!item) {
+				console.log("Could not find Reserve");
+				return;
+			}
+			item.remove();
+		});
+	} catch(err) {
+		console.log("Error", err);
+		// throw(err);
+	}
 });
 
 module.exports = mongoose.model('Booking', BookingSchema);
