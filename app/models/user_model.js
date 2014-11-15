a4515d90ac28822b9088b67ee8140252e970b6d8
Jason Norwood-Young <jason@10layer.com>
2014-11-15 13:37:19 +0200

    useradmin model

--- app/models/user_model.js
+++ app/models/user_model.js
@@ -5,6 +5,7 @@ var Objectid = mongoose.Schema.Types.ObjectId;
 
 var UserSchema   = new Schema({
 	name: String,
+	company: String,
 	email: { type: String, unique: true, index: true },
 	password: String,
 	admin: Boolean,
@@ -16,12 +17,46 @@ var UserSchema   = new Schema({
 	stuff_reserve: Number,
 	stuff_debit: Number,
 	stuff_credit: Number,
+	twitter: String,
+	facebook: String,
+	google: String,
+	linkedin: String,
+	skype: String,
+	mobile: String,
+	about: String,
+	pic: { type: String, default: '/avatars/grey_avatar_1.png' },
+	start_date: { type: Date, default: Date.now },
+	referee: String,
+	referal_method: String,
+	status: { type: String, validate: /active|inactive/, index: true, default: "inactive" },
+	_owner_id: Objectid,
 });
 
 UserSchema.set("_perms", {
-	admin: "crud",
-	owner: "crud",
+	admin: "cru",
+	owner: "cru",
 	user: "r",
+});
+
+UserSchema.post("save", function(user) { 
+	var Useradmin 	= require('./useradmin_model');
+	Useradmin.findOne({ user_id: user._id }, function(err, useradmin) {
+		if (err) {
+			console.log("Err", err);
+			return;
+		}
+		if (useradmin) {
+			console.log("Useradmin already exists, bailing");
+			return;
+		} else {
+			useradmin = Useradmin();
+			useradmin.user_id = user._id;
+			useradmin.extra_credits = 0;
+			useradmin._owner_id = user._id;
+			useradmin.save();
+			console.log("Created useradmin entry");
+		}
+	});
 });
 
 module.exports = mongoose.model('User', UserSchema);
