5f1d4699c148f48971a1ef9b70cba9d4df4fc57b
Jason Norwood-Young <jason@10layer.com>
2014-12-01 17:56:13 +0200

    More config options

--- config.js
+++ config.js
@@ -4,4 +4,9 @@ module.exports = {
 	port: "3001",
 	apikey_lifespan: 60 * 60 * 24,
 	secret: "totally#ecret",
+	smtp_server: "your.mail.server",
+	smtp_username: "you@your.mail.server",
+	smtp_password: "password",
+	smtp_from: "Administrator <you@your.mail.server>",
+	password_recovery_url: "http://localhost:3000/login/reset",
 }
