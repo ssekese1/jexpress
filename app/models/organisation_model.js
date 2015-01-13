e3201153e033a5830f96dc2b108f8b5d1f090674
Jason Norwood-Young <jason@10layer.com>
2015-01-13 15:30:48 +0200

    Some changes to org model

--- app/models/organisation_model.js
+++ app/models/organisation_model.js
@@ -6,13 +6,18 @@ var Objectid = mongoose.Schema.Types.ObjectId;
 var OrganisationSchema   = new Schema({
 	name: { type: String, unique: true, index: true },
 	tel: String,
+	mobile: String,
 	email: String,
 	website: String,
+	address: String,
 	twitter: String,
 	facebook: String,
 	linkedin: String,
 	img: String,
 	about: String,
+	user_id: Objectid,
+	sage_uid: Number,
+	vat: String,
 	_owner_id: Objectid
 });
 
