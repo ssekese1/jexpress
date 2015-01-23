5517e3911d9fcfac826cf3653c94406df5761830
Jason Norwood-Young <jason@10layer.com>
2015-01-23 21:40:21 +0200

    Membership model

---
+++ app/models/membership_model.js
@@ -0,0 +1,32 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var Objectid = mongoose.Schema.Types.ObjectId;
+
+var MembershipSchema   = new Schema({
+	name: String,
+	space_credits_per_month: Number,
+	stuff_credits_per_month: Number,
+	description: String,
+	cost: Number,
+	cost_period: String,
+	max_members: Number,
+	cost_extra_member: Number,
+	gigs: Number,
+	business_address: Boolean,
+	hotdesk: Boolean,
+	boardroom_access: Boolean,
+	free_printing: Boolean,
+	hotdesk_discount: Number,
+	boardroom_discount: Number,
+	_owner_id: Objectid,
+});
+
+MembershipSchema.set("_perms", {
+	admin: "crud",
+	owner: "r",
+	user: "r",
+	all: "r"
+});
+
+module.exports = mongoose.model('Membership', MembershipSchema);
