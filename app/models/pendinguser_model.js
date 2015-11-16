8091be1dfd0b55129090d59cb7e330f5fcba9320
Jason Norwood-Young <jason@10layer.com>
2015-11-16 15:02:45 +0200

    Make sure we can't book in next day and can't submit if there's a problem

--- app/models/pendinguser_model.js
+++ app/models/pendinguser_model.js
@@ -6,6 +6,7 @@ var Objectid = mongoose.Schema.Types.ObjectId;
 var PendinguserSchema   = new Schema({
 	name: String,
 	organisation: String,
+	organisation_id: { type: Objectid, ref: "Organisation" },
 	location_id: Objectid,
 	email: { type: String, unique: true, index: true },
 	mobile: String,
@@ -14,11 +15,32 @@ var PendinguserSchema   = new Schema({
 	referal_method: String,
 	url: String,
 	newsletter: Boolean,
+	otp: { type: String, get: function(otp) {
+		return "<obfuscated>";
+		} 
+	}
 });
 
 PendinguserSchema.set("_perms", {
 	admin: "crud",
+	owner: "crd",
+	user: "r",
 	all: "c",
 });
+
+
+
+// PendinguserSchema.statics.confirm(function(data) {
+// 	var User = require("./user_model");
+
+// });
+
+PendinguserSchema.pre("save", function(next) {
+	var pendinguser = this;
+	this.otp = require('rand-token').generate(16);
+	next();
+});
+
+
 
 module.exports = mongoose.model('Pendinguser', PendinguserSchema);
