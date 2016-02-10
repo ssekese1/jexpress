13d4804f0193e0ff97e4372a47496db50632ddd1
Jason Norwood-Young <jason@10layer.com>
2016-02-10 17:38:08 +0200

    Require location

--- app/models/license_model.js
+++ app/models/license_model.js
@@ -5,10 +5,12 @@ var Objectid = mongoose.Schema.Types.ObjectId;
 var Organisation = require("./organisation_model");
 var Membership = require("./membership_model");
 var User = require("./user_model");
+var Location = require("./location_model");
 
 var LicenseSchema   = new Schema({
 	organisation_id: { type: Objectid, ref: 'Organisation' },
 	membership_id: { type: Objectid, ref: 'Membership' },
+	location_id: { type: Objectid, ref: 'Location' },
 	user_id: { type: Objectid, ref: 'User' }
 });
 
