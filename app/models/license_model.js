54abe4cd073142397e371997f5857ac71cd6fddc
Jason Norwood-Young <jason@10layer.com>
2016-02-10 12:48:24 +0200

    License model

---
+++ app/models/license_model.js
@@ -0,0 +1,20 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var Objectid = mongoose.Schema.Types.ObjectId;
+var Organisation = require("./organisation_model");
+var Membership = require("./membership_model");
+var User = require("./user_model");
+
+var LicenseSchema   = new Schema({
+	organisation_id: { type: Objectid, ref: 'Organisation' },
+	membership_id: { type: Objectid, ref: 'Membership' },
+	user_id: { type: Objectid, ref: 'User' }
+});
+
+LicenseSchema.set("_perms", {
+	admin: "crud",
+	primary_member: "r"
+});
+
+module.exports = mongoose.model('License', LicenseSchema);
