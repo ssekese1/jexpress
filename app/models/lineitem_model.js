8676f6f9411da29c162b1334e530a0e6b0f5cb45
Jason Norwood-Young <jason@10layer.com>
2016-04-28 13:29:14 +0200

    Fix naming bug that was bombing out lineitem autopopulate

--- app/models/lineitem_model.js
+++ app/models/lineitem_model.js
@@ -3,13 +3,13 @@ var Schema       = mongoose.Schema;
 
 var Objectid = mongoose.Schema.Types.ObjectId;
 var Organisation = require("./organisation_model");
-var SageItem = require("./sageitem_model");
+var Sageitem = require("./sageitem_model");
 var SageAnalysisCategory = require("./sageanalysiscategory_model");
 
 var LineItemSchema   = new Schema({
 	description: String,
 	organisation_id: { type: Objectid, ref: 'Organisation' },
-	item: { type: Objectid, ref: 'SageItem' },
+	item: { type: Objectid, ref: 'Sageitem' },
 	amount: { type: Number, validate: function(v) { return (v > 0); }, required: true },
 	price: { type: Number, validate: function(v) { return (v >= 0); }, required: true },
 	tax_type: String,
@@ -18,7 +18,7 @@ var LineItemSchema   = new Schema({
 	date_created: { type: Date, default: Date.now },
 	sage_id: Number,
 	is_quote: Boolean,
-	analysiscategory: { type: Objectid, ref: 'SageAnalysisCategory' },
+	analysiscategory: { type: Objectid, ref: 'Sageanalysiscategory' },
 	_owner_id: Objectid,
 	_deleted: { type: Boolean, default: false, index: true },
 });
