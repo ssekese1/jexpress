3ba12ac50a52d7462e85f1f503259ccdec3e68f5
Jason Norwood-Young <jason@10layer.com>
2016-05-13 11:39:20 +0200

    Make test model a bit friendlier as an example

--- app/models/test_model.js
+++ app/models/test_model.js
@@ -1,26 +1,30 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var Objectid = mongoose.Schema.Types.ObjectId;
+// This is just a shortcut for us
+var ObjectId = mongoose.Schema.Types.ObjectId;
 var Mixed = mongoose.Schema.Types.Mixed;
 
 var TestSchema   = new Schema({
-	foo: String,
-	bar: String,
-	yack: Mixed,
-	shmack: [String],
-	_owner_id: Objectid
+    foo: String, // A normal string
+    bar: { type: String, unique: true, index: true }, // Ah! Some business logic!
+    yack: Mixed, // We can put anything in here, including objects
+    shmack: [String], // We can store arrays
+    _owner_id: ObjectId // This is one of the magic fields that will be populated by the API
 });
 
+// We can set permissions for different user types and user groups
 TestSchema.set("_perms", {
-	admin: "crud",
-	owner: "crud",
-	user: "cr",
-	all: "r"
+    admin: "crud", // CRUD = Create, Retrieve, Update and Delete
+    owner: "crud",
+    user: "cr",
+    all: "r" // Unauthenticated users will be able to read from test, but that is all
 });
 
+// We can define useful functions that we can call through the API using our /call endpoint
 TestSchema.statics.test = function() {
-	return "Testing OKAY!";
-}
+    return "Testing OKAY!";
+};
 
+// Finally, we export our model. Make sure to change the name!
 module.exports = mongoose.model('Test', TestSchema);
