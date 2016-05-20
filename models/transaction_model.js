b2e6a2748b735deba265ea1e7a1a58676b488f36
Jason Norwood-Young <jason@10layer.com>
2016-05-20 13:17:10 +0200

    Some extra transaction table fields for Payfast

--- models/transaction_model.js
+++ models/transaction_model.js
@@ -11,9 +11,10 @@ var TransactionSchema   = new Schema({
 	user_id: { type: Objectid, index: true, required: true, ref: "User" },
 	organisation_id: { type: Objectid, index: true, ref: "Organisation" },
 	product_id: { type: Objectid, index: true, ref: "Product" },
+	name: String,
 	description: String,
 	date: { type: Date, default: Date.now },
-	amount: { type: Number, validate: function(v) { return (v > 0) }, required: true },
+	amount: { type: Number, validate: function(v) { return (v > 0); }, required: true },
 	cred_type: { type: String, validate: /space|stuff|bandwidth/, index: true, required: true },
 	transaction_status: Number,
 	result_code: Number,
@@ -21,13 +22,16 @@ var TransactionSchema   = new Schema({
 	result_desc: String,
 	transaction_id: Number,
 	risk_indicator: String,
+	pf_payment_id: Number,
+	payment_status: String,
+	amount_fee: Number,
+	amount_net: Number,
 	_owner_id: Objectid
 });
 
 TransactionSchema.set("_perms", {
 	admin: "crud",
-	owner: "ru",
-	user: "",
+	user: "c",
 });
 
 
