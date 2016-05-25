c331fc639312c4f0ebe65e550f4f486d9c8fa9e8
Jason Norwood-Young <jason@10layer.com>
2016-05-25 14:14:36 +0200

    Some notes

--- stresstest.js
+++ stresstest.js
@@ -5,6 +5,7 @@ var count = 5000;
 var queue = [];
 
 var testFunc = function(cb) {
+	// Add authentication to see a massive slow-down
 	rest.get("http://localhost:3001/api/location")
 	.then(function(result) {
 		cb(null);
@@ -18,9 +19,8 @@ for (var x = 0; x < count; x++) {
 	queue.push(testFunc);
 }
 var startTime = +new Date();
-console.time("StressTest");
+// change to parallel for some fun
 async.series(queue, function(result) {
-	console.timeEnd("StressTest");
 	var endTime = +new Date();
 	var diff = endTime - startTime;
 	console.log("Time: " + diff + "ms, Per Second: " + (count / (diff / 1000)));
