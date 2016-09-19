03091e1cb56f6d923bcc6dec295179be0d6b844e
Jason Norwood-Young <jason@10layer.com>
2016-09-19 12:17:16 +0200

    Invoice model

---
+++ models/invoice_model.js
@@ -0,0 +1,32 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var Location = require('./location_model');
+var Organisation = require("./organisation_model");
+
+var InvoiceSchema   = new Schema({
+	invoice_id: { type: String, index: true },
+	invoice_number: String,
+	organisation_id: { type: ObjectId, index: true, ref: "Organisation" },
+	location: { type: ObjectId, index: true, ref: "Location" },
+	date: Date,
+	due_date: Date,
+	sent: Boolean,
+	status: String,
+	sub_total: Number,
+	total: Number,
+	discount: Number,
+	tax: Number,
+	date_created: { type: Date, default: Date.now },
+	line_items: [ mongoose.Schema.Types.Mixed ],
+	_owner_id: ObjectId
+});
+
+InvoiceSchema.set("_perms", {
+	admin: "crud",
+	owner: "cr",
+	user: ""
+});
+
+module.exports = mongoose.model('Invoice', InvoiceSchema);
