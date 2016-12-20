8657af153e6cde0782d555120c72b6d7b43b6b87
Jason Norwood-Young <jason@10layer.com>
2016-12-20 08:35:49 +0200

    Include location in membership model and some cleanup

--- models/membership_model.js
+++ models/membership_model.js
@@ -1,15 +1,17 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var Objectid = mongoose.Schema.Types.ObjectId;
+var ObjectId = mongoose.Schema.Types.ObjectId;
 var Sageanalysiscategory = require("./sageanalysiscategory_model");
+var Location = require("./location_model");
 
 var MembershipSchema   = new Schema({
 	name: String,
+	location_id: { type: ObjectId, ref: 'Location' },
 	space_credits_per_month: Number,
 	stuff_credits_per_month: Number,
 	print_credits_per_month: Number,
-	// bandwidth_per_month: Number,
+	bandwidth_per_month: Number,
 	description: String,
 	marketing_description: String,
 	cost: Number,
@@ -18,9 +20,6 @@ var MembershipSchema   = new Schema({
 	cost_extra_member: Number,
 	space_credits_per_month_extra_member: Number,
 	stuff_credits_per_month_extra_member: Number,
-	sage_analytics_categories: mongoose.Schema.Types.Mixed,
-	// bandwidth_per_month_extra_member: Number,
-	gigs: Number,
 	business_address: Boolean,
 	hotdesk: Boolean,
 	boardroom_access: Boolean,
@@ -29,12 +28,11 @@ var MembershipSchema   = new Schema({
 	boardroom_discount: Number,
 	discount_multiplier: { type: Number, default: 1 },
 	special: Boolean,
-	sage: mongoose.Schema.Types.Mixed,
 	datatill_product: Number,
 	papercut_group: String,
 	xero_account: String,
 	xero_itemid: String,
-	_owner_id: Objectid,
+	_owner_id: ObjectId,
 	_deleted: { type: Boolean, default: false, index: true },
 });
 
