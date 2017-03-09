50e1c77c385395ad0d17556f95a72088a27b14d8
Jason Norwood-Young <jason@10layer.com>
2017-03-09 12:21:20 +0200

    Ensure partner_id is unique

--- models/partner_model.js
+++ models/partner_model.js
@@ -5,7 +5,7 @@ var friendly = require("mongoose-friendly");
 var PartnerSchema   = new Schema({
 	name: { type: String, required: true },
 	urlid: { type: String, unique: true, index: true },
-	partner_id: String,
+	partner_id: { type: String, unique: true },
 	date_created: { type: Date, default: Date.now },
 	_version: { type: Number, default: 0 },
 	_deleted: { type: Boolean, default: false, index: true },
