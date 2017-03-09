808b4a42857deb28a63309ae30d5daa85308961e
Jason Norwood-Young <jason@10layer.com>
2017-03-09 09:05:25 +0200

    Urlid and partner_id fields

--- models/partner_model.js
+++ models/partner_model.js
@@ -3,7 +3,9 @@ var Schema       = mongoose.Schema;
 var friendly = require("mongoose-friendly");
 
 var PartnerSchema   = new Schema({
-	name: String,
+	name: { type: String, required: true },
+	urlid: { type: String, unique: true, index: true },
+	partner_id: String,
 	date_created: { type: Date, default: Date.now },
 	_version: { type: Number, default: 0 },
 	_deleted: { type: Boolean, default: false, index: true },
