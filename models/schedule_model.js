21840917d2136651727d0649cab67b57e27da15a
Jason Norwood-Young <jason@10layer.com>
2017-03-28 14:58:31 +0200

    New schedule model

--- models/schedule_model.js
+++ models/schedule_model.js
@@ -5,11 +5,20 @@ var config		= require("config");
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var ScheduleSchema   = new Schema({
+	name: String,
+	description: String,
+	action: String,
+	due: { type: Date, index: true },
+	status: { type: String, validate: /due|running|run|cancelled|failed/, index: true, default: "due" },
+	last_run: Date,
+	last_output: mongoose.Schema.Types.Mixed,
+	repeat:  { type: String, validate: /never|minutely|hourly|daily|monthly/, index: true, default: "never" },
 	created: { type: Date, default: Date.now },
 });
 
 ScheduleSchema.set("_perms", {
-	//We can never change or view this directly
+	super_user: "cru",
+	admin: "r",
 });
 
 module.exports = mongoose.model('Schedule', ScheduleSchema);
