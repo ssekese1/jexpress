c34079352696e8485362cb2fa3d67014a97ac4e5
Jason Norwood-Young <jason@10layer.com>
2017-04-04 09:08:18 +0200

    Add lots of tests for booking

--- test/booking.js
+++ test/booking.js
@@ -1,6 +1,7 @@
 process.env.NODE_ENV = 'test';
 
 var Booking = require("../models/booking_model");
+var Room = require('../models/room_model');
 
 var chai = require('chai');
 var chaiHttp = require('chai-http');
@@ -10,12 +11,25 @@ var server = require("../bin/server");
 
 var init = require("./init");
 
+var hour = 60 * 60 * 1000;
+var time = new Date().getTime();
+
 chai.use(chaiHttp);
 
 describe('Booking', () => {
 	before(init.init);
 
-	beforeEach(done => {
+	var room = null;
+	before(done => {
+		Room.findOne({}, (err, result) => {
+			if (err)
+				throw(err);
+			room = result;
+			done();
+		});
+	});
+
+	before(done => {
 		Booking.remove({}, err => {
 			done();
 		});
@@ -39,13 +53,40 @@ describe('Booking', () => {
 		it("it should POST a new booking", (done) => {
 			var booking = {
 				title: "Test Booking",
-				start_time: new Date(),
-				end_time: new Date(),
+				start_time: new Date(time),
+				end_time: new Date(time + hour),
+				room: room._id
 			};
 			chai.request(server)
 			.post("/api/booking")
 			.send(booking)
 			.auth(init.user_account.email, init.user_account.password)
+			.end((err, res) => {
+				console.log(res.body);
+				res.should.have.status(200);
+				res.body.data.should.be.an('object');
+				res.body.data.should.have.property("_id");
+				res.body.data.should.have.property("title");
+				res.body.data.should.have.property("start_time");
+				res.body.data.should.have.property("end_time");
+				done();
+			});
+		});
+	});
+
+	describe("/POST booking - Admin", () => {
+		it("it should POST a new booking as an admin", (done) => {
+			var booking = {
+				title: "Test Booking",
+				start_time: new Date(time + hour),
+				end_time: new Date(time + (hour * 2)),
+				user_id: init.user_account._id,
+				room: room._id
+			};
+			chai.request(server)
+			.post("/api/booking")
+			.send(booking)
+			.auth(init.admin_account.email, init.admin_account.password)
 			.end((err, res) => {
 				res.should.have.status(200);
 				res.body.data.should.be.an('object');
@@ -58,5 +99,140 @@ describe('Booking', () => {
 		});
 	});
 
+	describe("/POST booking - Checks", () => {
+		it("it should fail without a room", (done) => {
+			var booking = {
+				title: "Test Booking",
+				start_time: new Date(time),
+				end_time: new Date(time + hour),
+				user_id: init.user_account._id
+			};
+			chai.request(server)
+			.post("/api/booking")
+			.send(booking)
+			.auth(init.admin_account.email, init.admin_account.password)
+			.end((err, res) => {
+				res.should.have.status(500);
+				done();
+			});
+		});
+		it("it should fail without a start time", (done) => {
+			var booking = {
+				title: "Test Booking",
+				room: room._id,
+				end_time: new Date(time + hour),
+				user_id: init.user_account._id
+			};
+			chai.request(server)
+			.post("/api/booking")
+			.send(booking)
+			.auth(init.admin_account.email, init.admin_account.password)
+			.end((err, res) => {
+				res.should.have.status(500);
+				done();
+			});
+		});
+		it("it should fail without an end time", (done) => {
+			var booking = {
+				title: "Test Booking",
+				room: room._id,
+				start_time: new Date(time + hour),
+				user_id: init.user_account._id
+			};
+			chai.request(server)
+			.post("/api/booking")
+			.send(booking)
+			.auth(init.admin_account.email, init.admin_account.password)
+			.end((err, res) => {
+				res.should.have.status(500);
+				done();
+			});
+		});
+		it("it should fail without a title", (done) => {
+			var booking = {
+				room: room._id,
+				start_time: new Date(time),
+				end_time: new Date(time + hour),
+				user_id: init.user_account._id
+			};
+			chai.request(server)
+			.post("/api/booking")
+			.send(booking)
+			.auth(init.admin_account.email, init.admin_account.password)
+			.end((err, res) => {
+				res.should.have.status(500);
+				done();
+			});
+		});
+		it("it should fail if start_time > end_time", (done) => {
+			var booking = {
+				title: "Test Booking",
+				room: room._id,
+				start_time: new Date(time + hour),
+				end_time: new Date(time),
+				user_id: init.user_account._id
+			};
+			chai.request(server)
+			.post("/api/booking")
+			.send(booking)
+			.auth(init.admin_account.email, init.admin_account.password)
+			.end((err, res) => {
+				res.should.have.status(500);
+				done();
+			});
+		});
+		it("it should fail if user tries to get another user to pay", (done) => {
+			var booking = {
+				title: "Test Booking",
+				room: room._id,
+				start_time: new Date(time),
+				end_time: new Date(time + hour),
+				user: init.admin_account._id,
+			};
+			chai.request(server)
+			.post("/api/booking")
+			.send(booking)
+			.auth(init.user_account.email, init.user_account.password)
+			.end((err, res) => {
+				res.should.have.status(500);
+				done();
+			});
+		});
+	});
+
+	describe("/POST booking - Checks", () => {
+		it("it should post an appointment", (done) => {
+			var booking = {
+				title: "Test Booking",
+				room: room._id,
+				start_time: new Date(time + (hour * 2)),
+				end_time: new Date(time + (hour * 3)),
+			};
+			chai.request(server)
+			.post("/api/booking")
+			.send(booking)
+			.auth(init.admin_account.email, init.admin_account.password)
+			.end((err, res) => {
+				res.should.have.status(200);
+				done();
+			});
+		});
+		it("it should fail because an appointment already exists", (done) => {
+			var booking = {
+				title: "Test Booking",
+				room: room._id,
+				start_time: new Date(time),
+				end_time: new Date(time + hour),
+			};
+			chai.request(server)
+			.post("/api/booking")
+			.send(booking)
+			.auth(init.user_account.email, init.user_account.password)
+			.end((err, res) => {
+				res.should.have.status(500);
+				done();
+			});
+		});
+	});
 
 });
