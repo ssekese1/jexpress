b98d12bebd3f5b57b1f87c61d5fa87975f7ffee9
Jason Norwood-Young <jason@10layer.com>
2017-04-24 12:45:02 +0200

    Create spacetype model and link to space model

--- models/space_model.js
+++ models/space_model.js
@@ -3,12 +3,13 @@ var Schema       = mongoose.Schema;
 
 var ObjectId = mongoose.Schema.Types.ObjectId;
 var Location = require("./location_model");
+var SpaceType = require("./spacetype_model");
 
 var SpaceSchema   = new Schema({
 	name: String,
 	location_id: { type: ObjectId, ref: 'Location' },
-	xero_account: String,
 	meters_squared: Number,
+	spacetype_id: { type: ObjectId, ref: 'SpaceType' },
 	date_created: { type: Date, default: Date.now },
 	_owner_id: ObjectId,
 	_deleted: { type: Boolean, default: false, index: true },
---
+++ models/spacetype_model.js
@@ -0,0 +1,21 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var SpaceTypeSchema   = new Schema({
+	name: String,
+	xero_account: String,
+	date_created: { type: Date, default: Date.now },
+	_owner_id: ObjectId,
+	_deleted: { type: Boolean, default: false, index: true },
+});
+
+SpaceTypeSchema.set("_perms", {
+	admin: "crud",
+	owner: "r",
+	user: "r",
+	all: "r"
+});
+
+module.exports = mongoose.model('SpaceType', SpaceTypeSchema);
