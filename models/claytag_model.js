feb8691e7ee146cc22ac6b00a97005fd712ef383
Jason Norwood-Young <jason@10layer.com>
2017-05-02 13:56:49 +0200

    Claytag model

---
+++ models/claytag_model.js
@@ -0,0 +1,22 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var Location = require('./location_model');
+var User = require("./user_model");
+
+var ClaytagSchema   = new Schema({
+	id: { type: String, index: true },
+	number: Number,
+	is_assigned: Boolean,
+	system: mongoose.Schema.Types.Mixed,
+	location_id: { type: ObjectId, index: true, ref: "Location" },
+	date_created: { type: Date, default: Date.now },
+	_owner_id: ObjectId
+});
+
+ClaytagSchema.set("_perms", {
+	admin: "crud",
+});
+
+module.exports = mongoose.model('Claytag', ClaytagSchema);
--- models/license_model.js
+++ models/license_model.js
@@ -1,22 +1,24 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var Objectid = mongoose.Schema.Types.ObjectId;
+var ObjectId = mongoose.Schema.Types.ObjectId;
 var Organisation = require("./organisation_model");
 var Membership = require("./membership_model");
 var User = require("./user_model");
 var Location = require("./location_model");
+var Claytag = require("./claytag_model");
 var diff = require('deep-diff').diff;
 var Log = require("./log_model");
 
 var LicenseSchema   = new Schema({
-	organisation_id: { type: Objectid, ref: 'Organisation', index: true, required: true },
-	membership_id: { type: Objectid, ref: 'Membership', required: true },
-	location_id: { type: Objectid, ref: 'Location', required: true },
+	organisation_id: { type: ObjectId, ref: 'Organisation', index: true, required: true },
+	membership_id: { type: ObjectId, ref: 'Membership', required: true },
+	location_id: { type: ObjectId, ref: 'Location', required: true },
 	xero_account: String,
-	user_id: { type: Objectid, ref: 'User', index: true },
+	user_id: { type: ObjectId, ref: 'User', index: true },
+	claytag_id: { type: ObjectId, ref: "Claytag" },
 	date_created: { type: Date, default: Date.now },
-	_owner_id: Objectid,
+	_owner_id: ObjectId,
 	_deleted: { type: Boolean, default: false, index: true },
 });
 
