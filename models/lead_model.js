fa96a9cf84cd3bbddafdd6fc0321e48450d1d73e
Jason Norwood-Young <jason@10layer.com>
2017-05-11 13:55:44 +0200

    Lead model

---
+++ models/lead_model.js
@@ -0,0 +1,23 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var Location = require('./location_model');
+
+var LeadSchema   = new Schema({
+	name: { type: String, index: true },
+	organisation: String,
+	location_id: { type: ObjectId, index: true, ref: "Location" },
+	email: { type: String, index: true },
+	mobile: String,
+	date_created: { type: Date, default: Date.now },
+	source: String,
+});
+
+LeadSchema.set("_perms", {
+	admin: "crud",
+	user: "cr",
+	all: "c",
+});
+
+module.exports = mongoose.model('Lead', LeadSchema);
