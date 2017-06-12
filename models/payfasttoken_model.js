363d01063c3d02bf7f319658a83caacfa6171863
Jason Norwood-Young <jason@10layer.com>
2017-06-12 12:01:58 +0200

    Ensure only superusers can create payfast tokens and that the user can retrieve his

--- models/payfasttoken_model.js
+++ models/payfasttoken_model.js
@@ -6,16 +6,17 @@ var User = require("./user_model");
 var Organisation = require("./organisation_model");
 
 var PayfasttokenSchema   = new Schema({
-	token: String,
-	user_id: { type: ObjectId, ref: 'User' },
-	organisation_id: { type: ObjectId, ref: 'Organisation' },
+	token: { type: String, index: true, unique: true },
+	user_id: { type: ObjectId, ref: 'User', index: true },
+	organisation_id: { type: ObjectId, ref: 'Organisation', index: true },
 	date_created: { type: Date, default: Date.now },
 	_owner_id: ObjectId,
 	_deleted: { type: Boolean, default: false, index: true },
 });
 
 PayfasttokenSchema.set("_perms", {
-	admin: "crud",
+	super_user: "crud",
+	admin: "r",
 	owner: "r",
 });
 
