005bc4fb9fb8baa3939d86f115fc1c930f7aa75d
Jason Norwood-Young <jason@10layer.com>
2017-06-15 16:29:57 +0200

    Change some permissions and store some more details from Payfast

--- models/payfasttoken_model.js
+++ models/payfasttoken_model.js
@@ -2,6 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var ObjectId = mongoose.Schema.Types.ObjectId;
+var Mixed = mongoose.Schema.Types.Mixed;
 var User = require("./user_model");
 var Organisation = require("./organisation_model");
 
@@ -11,15 +12,18 @@ var PayfasttokenSchema   = new Schema({
 	organisation_id: { type: ObjectId, ref: 'Organisation', index: true },
 	card_number: String,
 	expiration_date: { type: Date, index: true },
+	name_first: String,
+	name_last: String,
+	email: String,
+	payfast_response: Mixed,
 	date_created: { type: Date, default: Date.now },
 	_owner_id: ObjectId,
 	_deleted: { type: Boolean, default: false, index: true },
 });
 
 PayfasttokenSchema.set("_perms", {
-	super_user: "crud",
-	admin: "r",
-	owner: "r",
+	admin: "crud",
+	owner: "rd",
 });
 
 
