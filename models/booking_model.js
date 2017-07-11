b2c3e7a8981396e2080a53f64674c98cebbf9c0b
Jason Norwood-Young <jason@10layer.com>
2017-07-11 16:47:42 +0200

    radius_username, radius_password

--- models/booking_model.js
+++ models/booking_model.js
@@ -31,6 +31,8 @@ var BookingSchema   = new Schema({
 	layout: { type: ObjectId, ref: "Layout" },
 	booking_url: String,
 	website: String,
+	radius_username: String,
+	radius_password: String,
 	_owner_id: ObjectId,
 	_deleted: { type: Boolean, default: false, index: true },
 	_version: { type: Number, default: 0 },
