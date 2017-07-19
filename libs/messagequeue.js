d4f4410bd9dd4482567fc7f8f8b97bcee1983506
Jason Norwood-Young <jason@10layer.com>
2017-07-19 13:54:03 +0200

    Dump RabbitMQ

--- libs/messagequeue.js
+++ libs/messagequeue.js
@@ -1,24 +1,9 @@
 var config = require("config");
-var ampq = require('amqplib');
-
-var connection = ampq.connect(config.rabbitmq.server);
+var restler = require("restler-q");
 
 var MessageQueue = {
 	action: function(type, op, user, item) {
-		return connection
-		.then(function(conn) {
-			return conn.createChannel();
-		})
-		.then(ch => {
-			ch.assertQueue(config.rabbitmq.queue, {durable: true});
-			var data = { type: type, op: op, params: [ user, item ], timestamp: +new Date() };
-			// console.log("RabbitMQ data", JSON.stringify(data));
-			return ch.sendToQueue(config.rabbitmq.queue, new Buffer(JSON.stringify(data)));
-		})
-		.then(null, function(err) {
-			console.error("RabbitMQ error", err);
-		})
-		;
+		return restler.post(`${ config.open_queue.server }/action`, { username: config.open_queue.username, password: config.open_queue.password, data: { type, op, params: JSON.stringify([ user, item ]) } });
 	}
 };
 
