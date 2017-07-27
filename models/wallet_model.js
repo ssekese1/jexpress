bb1666797c8ac292673b8954072650981fe72e61
Jason Norwood-Young <jason@10layer.com>
2017-07-27 10:28:53 +0200

    Multiple users, use the word quota, some defaults

--- models/wallet_model.js
+++ models/wallet_model.js
@@ -10,19 +10,19 @@ var WalletSchema   = new Schema({
 	name: String,
 	currency_id: { type: ObjectId, index: true, ref: "Currency" },
 	priority: Number,
-	topup_frequency: { type: String, validate: /daily|weekly|monthly|annually|never/, index: true, default: "never" },
-	topup_amount: Number,
-	last_topup: { type: Date, index: true },
-	user_id: { type: ObjectId, index: true, ref: "User" },
+	quota_frequency: { type: String, validate: /daily|weekly|monthly|annually|never/, index: true, default: "never" },
+	quota_amount: Number,
+	last_quota_date: { type: Date, index: true },
+	user_id: [{ type: ObjectId, index: true, ref: "User" }],
 	organisation_id: { type: ObjectId, index: true, ref: "Organisation" },
-	balance: Number,
+	balance: { type: Number, default: 0 },
 	date_created: { type: Date, default: Date.now },
 	_owner_id: ObjectId
 });
 
 WalletSchema.set("_perms", {
 	admin: "crud",
-	owner: "r",
+	user: "r",
 });
 
 module.exports = mongoose.model('Wallet', WalletSchema);
