fca930b0955bbeec29143d842f89b51b25d1a41a
Jason Norwood-Young <jason@10layer.com>
2017-11-29 14:08:18 +0200

    Make presence default to true

--- models/user_model.js
+++ models/user_model.js
@@ -46,7 +46,7 @@ var UserSchema   = new Schema({
 	referal_method: String,
 	status: { type: String, validate: /active|inactive/, index: true, default: "inactive" },
 	hidden: { type: Boolean, default: false, index: true },
-	presence: { type: Boolean, default: false, index: true },
+	presence: { type: Boolean, default: true, index: true },
 	newsletter: Boolean,
 	radius_id: Number,
 	pin: String,
