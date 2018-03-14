ccd72e30272ae232c0f78ebe0872fa0cc6d744bc
Jason Norwood-Young <jason@10layer.com>
2018-03-14 12:14:02 +0200

    Xero Org Account

---
+++ models/xeroorgaccount_model.js
@@ -0,0 +1,21 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+var Location = require('./location_model');
+var Organisation = require('./organisation_model');
+
+var XeroOrgAccountSchema   = new Schema({
+	location_id: { type: ObjectId, ref: 'Location' },
+	organisation_id: { type: ObjectId, ref: 'Organisation', index: true },
+	xero_id: String
+});
+
+XeroOrgAccountSchema.set("_perms", {
+	super_user: "crud",
+	admin: "crud",
+	user: "r",
+	all: ""
+});
+
+module.exports = mongoose.model('XeroOrgAccount', XeroOrgAccountSchema);
