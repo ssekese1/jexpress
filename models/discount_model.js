4ee0a06c5b008770b5d8176644ab0af717fbbec3
Jason Norwood-Young <jason@10layer.com>
2018-10-04 17:45:20 +0200

    Discounts!

---
+++ models/discount_model.js
@@ -0,0 +1,25 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+var ObjectId     = mongoose.Schema.Types.ObjectId;
+var Organisation = require("./organisation_model");
+var LineItem     = require("./lineitem_model");
+
+var DiscountSchema   = new Schema({
+	date_created: { type: Date, default: Date.now },
+    discount: { type: Number, default: 0 },
+    description: String,
+    organisation_id: { type: ObjectId, ref: "Organisation" },
+    lineitem_id: { type: ObjectId, ref: "LineItem" }, // If left blank, apply to entire organisation
+    date_start: { type: Date, default: Date.now },
+    date_end: Date,
+    apply_to: [ { type: String, validate: /product|license|booking|all/ } ], // For organisation-wide discounts
+	_owner_id: ObjectId,
+	_version: { type: Number, default: 0 },
+	_deleted: { type: Boolean, default: false, index: true },
+});
+
+DiscountSchema.set("_perms", {
+	admin: "crud",
+});
+
+module.exports = mongoose.model('Discount', DiscountSchema);
--- models/lineitem_model.js
+++ models/lineitem_model.js
@@ -9,6 +9,8 @@ var Booking = require("./booking_model");
 var License = require("./license_model");
 var Location = require("./location_model");
 var User = require("./user_model");
+var Discount = require("./discount_model");
+var postFind 	 = require('../libs/mongoose-post-find');
 
 var LineItemSchema = new Schema({
 	description: String,
@@ -39,14 +41,23 @@ var LineItemSchema = new Schema({
 	price_customised_date: Date,
 	tax_type: String,
 	comment: String,
-	discount: { type: Number, default: 0 },
+	discounts: [ { type: ObjectId, ref: "Discount" } ],
+	// discount: { type: Number, default: 0 },
 	date_created: { type: Date, default: Date.now },
 	is_quote: Boolean,
 	xero_id: String,
 	date_start: Date,
 	date_end: Date,
 	_owner_id: ObjectId,
-	_deleted: { type: Boolean, default: false, index: true }
+	_deleted: { type: Boolean, default: false, index: true },
+	_version: { type: Number, default: 0 },
+}, {
+	toObject: {
+		virtuals: true
+	},
+	toJSON: {
+		virtuals: true
+	}
 });
 
 LineItemSchema.set("_perms", {
@@ -66,6 +77,87 @@ LineItemSchema.virtual("status").get(function() {
 	var date_end = +new Date(this.date_end);
 	if (date_end && date_end < now) return "expired";
 	return "current";
+});
+
+LineItemSchema.plugin(postFind, {
+	find: function(rows, done) {
+		Discount.find()
+		.then(discounts => {
+			rows.forEach(row => {
+				row._doc.discount = 0;
+				var org_discounts = discounts.filter(discount => discount.organisation_id + "" === row.organisation_id + "");
+				if (!org_discounts.length) return;
+				var lineitem_discounts = [];
+				for (discount of org_discounts) {
+					if (discount.lineitem_id && discount.lineitem_id === row.id) {
+						lineitem_discounts.push(discount);
+					} else if (discount.apply_to.includes("all")) {
+						lineitem_discounts.push(discount);
+					} else if (discount.apply_to.includes("product") && row.product_id) {
+						lineitem_discounts.push(discount);
+					} else if (discount.apply_to.includes("license") && row.license_id) {
+						lineitem_discounts.push(discount);
+					} else if (discount.apply_to.includes("booking") && row.booking_id) {
+						lineitem_discounts.push(discount);
+					}
+				}
+				// console.log({ lineitem_discounts });
+				row._doc.discounts = lineitem_discounts.map(discount => discount._id);
+				row._doc.discount = lineitem_discounts.reduce((sum, b) => ( sum + b.discount ), 0);
+			});
+			done(null, rows);
+		})
+		.catch(err => {
+			console.error(err);
+			done(err);
+		});
+	},
+
+	findOne: function(row, done) {
+		Discount.find({ organisation_id: row.organisation_id })
+		.then(discounts => {
+			console.log(row);
+			row._doc.discount = 0;
+			var org_discounts = discounts.filter(discount => discount.organisation_id + "" === row.organisation_id + "");
+			if (!org_discounts.length) {
+				return done(null, row);
+			}
+			var lineitem_discounts = [];
+			for (discount of org_discounts) {
+				if (discount.lineitem_id && discount.lineitem_id === row.id) {
+					lineitem_discounts.push(discount);
+				} else if (discount.apply_to.includes("all")) {
+					lineitem_discounts.push(discount);
+				} else if (discount.apply_to.includes("product") && row.product_id) {
+					lineitem_discounts.push(discount);
+				} else if (discount.apply_to.includes("license") && row.license_id) {
+					lineitem_discounts.push(discount);
+				} else if (discount.apply_to.includes("booking") && row.booking_id) {
+					lineitem_discounts.push(discount);
+				}
+			}
+			// console.log({ lineitem_discounts });
+			row._doc.discounts = lineitem_discounts.map(discount => discount._id);
+			row._doc.discount = lineitem_discounts.reduce((sum, b) => ( sum + b.discount ), 0);
+			done(null, row);
+		})
+		.catch(err => {
+			console.error(err);
+			done(err);
+		});
+		// Currency.findOne({ name: row.cred_type[0].toUpperCase() + row.cred_type.slice(1) })
+		// .then(currency => {
+		// 	return Wallet.find({ user_id: row.user_id, currency_id: currency._id });
+		// })
+		// .then(wallets => {
+		// 	row._doc.balance = wallets.reduce((sum, b) => ( sum + b.balance ), 0);
+		// 	done(null, row);
+		// })
+		// .catch(err => {
+		// 	console.error(err);
+		// 	done(err);
+		// });
+	}
 });
 
 // Check if we've changed the value of the product or license
