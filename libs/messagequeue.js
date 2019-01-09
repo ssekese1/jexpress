8a643521dbb2bf95609f6a5219c884fc6ca6f940
Jason Norwood-Young <jason@10layer.com>
2019-01-09 12:31:31 +0100

    Replace queue system with Google Pub/Sub

--- libs/messagequeue.js
+++ libs/messagequeue.js
@@ -1,9 +1,28 @@
 var config = require("config");
-var restler = require("restler-q");
+const {PubSub} = require('@google-cloud/pubsub');
+// Instantiates a client
+const pubsub = new PubSub({
+	projectId: config.google.projectId,
+	keyFilename: config.google.keyFilename
+});
+// The name for the new topic
+const topicName = config.google.topicName;
 
 var MessageQueue = {
-	action: function(type, op, user, item) {
-		return restler.post(`${ config.open_queue.server }/action`, { username: config.open_queue.username, password: config.open_queue.password, data: { type, op, params: JSON.stringify([ user, item ]) } });
+	action: async (type, op, user, item) => {
+		try {
+			const data = JSON.stringify([ user, item ]);
+			const dataBuffer = Buffer.from(data);
+			const messageId = await pubsub
+				.topic(topicName)
+				.publisher()
+				.publish(dataBuffer, { type, op, _id: item._id + "" });
+			console.log(`${ new Date() } Message ${messageId} published.`);
+			return messageId;
+		} catch(err) {
+			console.error(err);
+			return Promise.reject();
+		}
 	}
 };
 
