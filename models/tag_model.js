0b4f63546f5360d68fc8a3d238407d4e3005344b
Jason Norwood-Young <jason@10layer.com>
2019-03-25 11:39:04 +0100

    Ensure type + name = unique

--- models/tag_model.js
+++ models/tag_model.js
@@ -5,11 +5,13 @@ var Objectid = mongoose.Schema.Types.ObjectId;
 var Mixed = mongoose.Schema.Types.Mixed;
 
 var TagSchema   = new Schema({
-	name: { type: String, unique: true, index: true, set: toLower },
+	name: { type: String, index: true, set: toLower },
 	type: { type: String, index: true }
 }, {
 	timestamps: true
 });
+
+TagSchema.index({name: 1, type: 1}, {unique: true});
 
 TagSchema.set("_perms", {
 	admin: "crud",
