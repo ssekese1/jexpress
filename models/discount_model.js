5d242976c387ec5ca830c9735aa1ea66096236b4
Jason Norwood-Young <jason@10layer.com>
2019-05-22 15:15:21 +0200

    Add producttype to discount

--- models/discount_model.js
+++ models/discount_model.js
@@ -3,12 +3,14 @@ var Schema       = mongoose.Schema;
 var ObjectId     = mongoose.Schema.Types.ObjectId;
 var Organisation = require("./organisation_model");
 var LineItem     = require("./lineitem_model");
+var ProductType     = require("./producttype_model");
 
 var DiscountSchema   = new Schema({
 	date_created: { type: Date, default: Date.now },
     discount: { type: Number, default: 0 },
     description: String,
     organisation_id: { type: ObjectId, ref: "Organisation" },
+    producttype_id: { type: ObjectId, ref: "ProductType", default: null, index: true },
     lineitem_id: { type: ObjectId, ref: "LineItem" }, // If left blank, apply to entire organisation
     date_start: { type: Date, default: Date.now },
     date_end: Date,
