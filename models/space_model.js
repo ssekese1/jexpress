c60174fee810e76a5c22ef2684716eba487d2c4b
Jason Norwood-Young <jason@10layer.com>
2019-07-04 16:00:31 +0200

    Delete some crap and multiple rooms

--- models/space_model.js
+++ models/space_model.js
@@ -5,31 +5,14 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 var Location = require("./location_model");
 var License = require("./license_model");
 var SpaceType = require("./spacetype_model");
-var Product = require("./product_model");
+var Room = require("./room_model");
 var Claylock = require("./claylock_model");
 
 var SpaceSchema   = new Schema({
 	name: String,
-	location_id: { type: ObjectId, ref: 'Location' },
-	meters_squared: Number,
 	spacetype_id: { type: ObjectId, ref: 'SpaceType', required: true },
-	product_id: { type: ObjectId, ref: 'Product' },
+	room_id: [{ type: ObjectId, ref: 'Room' }],
 	claylock_id: [{ type: ObjectId, ref: 'Claylock' }],
-	date_created: { type: Date, default: Date.now },
-	seats: Number,
-	hot_oversell: Number,
-	occasional_oversell: Number,
-	shared: { type: Boolean, default: true },
-	budget_price: Number, // Use this to calculate individual line item amounts
-	aesthetic: String,
-	aesthetic_impact: Number,
-	business_case: Number,
-	business_case_date: Date,
-	business_case_factor: Number,
-	market_rate: Number,
-	market_date: Date,
-	twelve_month_discount: Number,
-	tenant_installation_allowance: Number,
 	_owner_id: ObjectId,
 	_deleted: { type: Boolean, default: false, index: true },
 }, {
@@ -49,14 +32,16 @@ SpaceSchema.set("_perms", {
 	all: "r"
 });
 
-SpaceSchema.index( { "$**": "text" } );
-
-SpaceSchema.virtual("budget_value_per_m2").get(function() {
-	return this.budget_price/this.meters_squared;
+SpaceSchema.virtual("total_value").get(function() {
+	return 0;
 });
 
-SpaceSchema.virtual("actual_value_per_m2").get(function() {
-	return this.actual_price/this.meters_squared;
+SpaceSchema.virtual("meters_squared").get(function() {
+	return 0;
+});
+
+SpaceSchema.virtual("seats").get(function () {
+	return 0;
 });
 
 module.exports = mongoose.model('Space', SpaceSchema);
